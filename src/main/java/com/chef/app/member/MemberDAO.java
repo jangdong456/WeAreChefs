f72bd371b68e6ee39428027b2f9be72c8364011f
jangdongguk <jangdong456@gmail.com>
2024-08-05 06:57:46 +0900

    1

---
+++ src/main/java/com/chef/app/member/MemberDAO.java
@@ -0,0 +1,12 @@
+package com.chef.app.member;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberDAO {
+	
+	public void kakao() {
+		System.out.println("== Kakao DAO ==");
+	}
+	
+}
---
+++ src/main/java/com/chef/app/member/MemberService.java
@@ -0,0 +1,17 @@
+package com.chef.app.member;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+	
+	@Autowired
+	private MemberDAO memberDAO;
+	
+	public void kakao() {
+		System.out.println("== Kakao Service ==");
+		memberDAO.kakao();
+		
+	}
+}
