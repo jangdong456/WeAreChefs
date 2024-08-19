df3ddb23881b52b7b87c76ca56944b91ba05102a
SSERYUNG <ksr0524@naver.com>
2024-08-19 10:47:26 +0900

    gogo

--- src/main/java/com/chef/app/member/MemberService.java
+++ src/main/java/com/chef/app/member/MemberService.java
@@ -43,7 +43,7 @@ public class MemberService {
 			result = memberDAO.kakaoCheck(memberDTO);
 			
 			// result이 1이면 회원가입된 상태 | 0이면 가입안된 상태
-			if(result < 0) {
+			if(result <= 0) {
 				// 가입된 회원이 없어서 kakaologin(생성하는 메서드 실행)
 				result = memberDAO.kakaologin(memberDTO);
 			} else {
